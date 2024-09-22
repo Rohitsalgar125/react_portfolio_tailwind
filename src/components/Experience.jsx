8c7161b26eab904da3ec7c2114c1e321519eafe5
Rohitsalgar125 <rohitsalgar1255@gmail.com>
2024-09-22 12:18:57 +0530

    added experiece

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import { EXPERIENCES } from "../constants";
 
 const Experience = () => {
   return (
-    <div>
-      experience
+    <div className="border-b border-neutral-900 pb-4">
+      <h1 className="text-center text-4xl my-20">Experience</h1>
+      <div>
+        {EXPERIENCES?.map((experience, index) => {
+          return (
+            <div
+              key={index}
+              className="mb-8 flex flex-wrap lg:justify-center"
+            >
+                <div className="w-full lg:w-1/4 ">
+                    <p className="mb-2 text-sm text-neutral-400">{experience.year}</p>
+                </div>
+                <div className="w-full max-x-xl lg:w-3/4">
+                    <h6 className="mb-2 font-semibold ">{experience.role} - <span className="text-sm text-purple-100">{experience.company}</span></h6>
+                    <p className="mb-4 text-neutral-400 ">{experience.description}</p>
+                    {
+                        experience.technologies?.map((tech,i)=>{
+                            return <span className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800" key={i}>{tech}</span>
+                        })
+                    }
+                </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Experience
+export default Experience;
