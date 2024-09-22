b2d6266eb7b43740bff8552b9fe09ee2e781eba6
Rohitsalgar125 <rohitsalgar1255@gmail.com>
2024-09-22 13:46:32 +0530

    anitamtion changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,7 @@
-import Aboutme from '../assets/about.jpg'
-import { ABOUT_TEXT } from '../constants';
+import { motion } from "framer-motion";
+import Aboutme from "../assets/about.jpg";
+import { ABOUT_TEXT } from "../constants";
+
 const About = () => {
   return (
     <div className=" border-b border-neutral-900 pb-4 ">
@@ -7,16 +9,25 @@ const About = () => {
         About <span className="text-neutral-500"> Me</span>
       </h1>
       <div className="flex flex-wrap ">
-        <div className="w-full lg:w-1/2 lg:p-8 ">
-            <div className="flex items-center justify-center">
-                <img className='rounded-2xl' src={Aboutme}  alt="about"/>
-            </div>
-        </div>
-            <div className='w-full lg:w-1/2 '>
-                <div className='flex justify-center lg:justify-start'>
-                    <p className='my-2 max-w-xl py-6 '>{ABOUT_TEXT}</p>
-                </div>
-            </div>
+        <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 0.5 }}
+          className="w-full lg:w-1/2 lg:p-8 "
+        >
+          <div className="flex items-center justify-center">
+            <img className="rounded-2xl" src={Aboutme} alt="about" />
+          </div>
+        </motion.div>
+        <motion.div className="w-full lg:w-1/2"
+         whileInView={{opacity :1 , x : 0}}
+         initial={{opacity :0 , x : 100}}
+         transition={{duration:0.5}}
+        >
+          <div className="flex justify-center lg:justify-start">
+            <p className="my-2 max-w-xl py-6 ">{ABOUT_TEXT}</p>
+          </div>
+        </motion.div>
       </div>
     </div>
   );
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,19 +1,39 @@
-import React from 'react'
-import { CONTACT } from '../constants'
+import React from "react";
+import { CONTACT } from "../constants";
+import { motion } from "framer-motion";
 
 const Contact = () => {
   return (
-    <div className='border-b border-neutral-700 pb-20'>
-    <h1 className='my-10 text-center text-4xl'>
-      Get in Touch
-    </h1>
-    <div className='text-center tracking-tighter'>
-     <p className='my-4 bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text:4xl tracking-tight text-transparent'>{CONTACT.address}</p>
-     <p className='my-4 bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text:4xl tracking-tight text-transparent'>{CONTACT.phoneNo}</p>
-     <a href='#' className='my-4 bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text:4xl tracking-tight text-transparent'>{CONTACT.email}</a>
+    <div className="border-b border-neutral-700 pb-20">
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-10 text-center text-4xl"
+      >
+        Get in Touch
+      </motion.h1>
+      <motion.div
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: 100 }}
+        transition={{ duration: 0.5 }}
+        className="text-center tracking-tighter"
+      >
+        <p className="my-4 bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text:4xl tracking-tight text-transparent">
+          {CONTACT.address}
+        </p>
+        <p className="my-4 bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text:4xl tracking-tight text-transparent">
+          {CONTACT.phoneNo}
+        </p>
+        <a
+          href="#"
+          className="my-4 bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text:4xl tracking-tight text-transparent"
+        >
+          {CONTACT.email}
+        </a>
+      </motion.div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
