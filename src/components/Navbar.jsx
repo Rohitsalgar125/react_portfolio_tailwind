83e3222a438c3c085b00a99e12a29d62047a9922
Rohitsalgar125 <rohitsalgar1255@gmail.com>
2024-09-22 13:58:08 +0530

    changed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,80 @@
-import React from 'react'
-import logo from '../assets/kevinRushLogo.png';
-import { FaLinkedin } from 'react-icons/fa';
-import { FaGithub } from 'react-icons/fa';
-import { FaInstagram } from 'react-icons/fa';
+import React from "react";
+import logo from "../assets/kevinRushLogo.png";
+import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
+  const linkVariants = {
+    initial: { opacity: 0, y: -10 },
+    animate: { opacity: 1, y: 0 },
+  };
+
   return (
-    <nav className='mb-20 flex justify-between items-center py-6'> 
-       <div className='flex flex-shrink-0 items-center'>
-        <img className='mx-2 w-10'  src={logo} alt='img'/>
-       </div>
-       <div className='m-8 flex justify-center items-center gap-4 text-2xl'>
-        <FaLinkedin/>
+    <nav className="mb-20 flex justify-between items-center py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-10" src={logo} alt="Logo" />
+      </div>
+      <div className="flex-grow flex justify-center items-center gap-8">
+        <motion.div
+          variants={linkVariants}
+          initial="initial"
+          animate="animate"
+          transition={{ duration: 0.5 }}
+        >
+          <a href="#home" className="text-2xl">
+            Home
+          </a>  
+        </motion.div>
+        <motion.div
+          variants={linkVariants}
+          initial="initial"
+          animate="animate"
+          transition={{ duration: 0.5, delay: 0.1 }}
+        >
+          <a href="#technologies" className="text-2xl">
+            Technologies
+          </a>
+        </motion.div>
+        <motion.div
+          variants={linkVariants}
+          initial="initial"
+          animate="animate"
+          transition={{ duration: 0.5, delay: 0.1 }}
+        >
+          <a href="#projects" className="text-2xl">
+            Projects
+          </a>
+        </motion.div>
+        <motion.div
+          variants={linkVariants}
+          initial="initial"
+          animate="animate"
+          transition={{ duration: 0.5, delay: 0.1 }}
+        >
+          <a href="#experience" className="text-2xl">
+            Experience 
+          </a>
+        </motion.div>
+        <motion.div
+          variants={linkVariants}
+          initial="initial"
+          animate="animate"
+          transition={{ duration: 0.5, delay: 0.1 }}
+        >
+          <a href="#contact" className="text-2xl">
+            Contact
+          </a>
+        </motion.div>
+        
+
+      </div>
+      <div className="flex gap-4 text-2xl">
+        <FaLinkedin />
         <FaGithub />
-        <FaInstagram/>
-
-
-       </div>
+        <FaInstagram />
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
