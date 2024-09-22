6d8f79d8579ef4638d4e1936e9f9c488375d7bcf
Rohitsalgar125 <rohitsalgar1255@gmail.com>
2024-09-22 13:51:32 +0530

    added animarion

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,9 +5,14 @@ import { ABOUT_TEXT } from "../constants";
 const About = () => {
   return (
     <div className=" border-b border-neutral-900 pb-4 ">
-      <h1 className="my-20 text-center text-4xl">
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-20 text-center text-4xl"
+      >
         About <span className="text-neutral-500"> Me</span>
-      </h1>
+      </motion.h1>
       <div className="flex flex-wrap ">
         <motion.div
           whileInView={{ opacity: 1, x: 0 }}
@@ -19,10 +24,11 @@ const About = () => {
             <img className="rounded-2xl" src={Aboutme} alt="about" />
           </div>
         </motion.div>
-        <motion.div className="w-full lg:w-1/2"
-         whileInView={{opacity :1 , x : 0}}
-         initial={{opacity :0 , x : 100}}
-         transition={{duration:0.5}}
+        <motion.div
+          className="w-full lg:w-1/2"
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 0.5 }}
         >
           <div className="flex justify-center lg:justify-start">
             <p className="my-2 max-w-xl py-6 ">{ABOUT_TEXT}</p>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -38,7 +38,7 @@ const Profile = () => {
               animate="visible"
               className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text:4xl tracking-tight text-transparent"
             >
-              Full Stack Developer
+              MERN Stack Developer
             </motion.span>
             <motion.p
               variants={container(1)}
@@ -52,9 +52,16 @@ const Profile = () => {
         </div>
         <div className="w-full lg:w-1/2 lg:p-8 ">
           <div className="flex justify-center ">
-            <motion.img initial={{x:100 , opacity : 0}} animate ={{
-              x : 0 , opacity : 1 
-            }} transition={{duration:1 , delay : 1.2}} src={profilepic} alt="profile" />
+            <motion.img
+              initial={{ x: 100, opacity: 0 }}
+              animate={{
+                x: 0,
+                opacity: 1,
+              }}
+              transition={{ duration: 1, delay: 1.2 }}
+              src={profilepic}
+              alt="profile"
+            />
           </div>
         </div>
       </div>
