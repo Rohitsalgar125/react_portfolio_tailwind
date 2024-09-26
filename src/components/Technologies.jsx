34924590f3ac46282fec3753eec7383ed71b33e8
Rohitsalgar125 <rohitsalgar1255@gmail.com>
2024-09-26 15:04:15 +0530

    technologies updated

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,9 +1,11 @@
 import { motion } from "framer-motion";
 import React from "react";
-import { FaNodeJs } from "react-icons/fa";
+import { DiJavascript } from "react-icons/di";
+import { FaCss3, FaGithub, FaHtml5, FaNodeJs, FaWindows } from "react-icons/fa";
 import { RiReactjsLine } from "react-icons/ri";
-import { SiMongodb } from "react-icons/si";
+import { SiExpress, SiMongodb } from "react-icons/si";
 import { TbBrandNextjs } from "react-icons/tb";
+import { VscVscode } from "react-icons/vsc";
 
 const iconVariants = (duration) => ({
   initial: { y: -10 },
@@ -41,6 +43,30 @@ const Technologies = () => {
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 "
         >
+          <FaHtml5 className="text-7xl text-orange-700" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 "
+        >
+          <FaCss3 className="text-7xl text-yellow-400" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 "
+        >
+          <DiJavascript className="text-7xl text-yellow-700" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 "
+        >
           <RiReactjsLine className="text-7xl text-cyan-700" />
         </motion.div>
         <motion.div
@@ -49,7 +75,7 @@ const Technologies = () => {
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 "
         >
-          <TbBrandNextjs className="text-7xl text-cyan-700" />
+          <TbBrandNextjs className="text-7xl text-black-700" />
         </motion.div>
         <motion.div
           variants={iconVariants(3.5)}
@@ -65,7 +91,63 @@ const Technologies = () => {
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 "
         >
+          <SiExpress className="text-7xl text-black-500" />
+        </motion.div>
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 "
+        >
           <FaNodeJs className="text-7xl text-green-500" />
+        </motion.div>
+      </motion.div>
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-col justify-center items-center gap-2 my-10"
+      >
+        <motion.h4
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 0.5 }}
+          className=" text-center text-2xl"
+        >
+          Tools
+        </motion.h4>
+        <motion.div
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 1.5 }}
+          className="flex flex-wrap items-center justify-center gap-4 m-4 "
+        >
+          <motion.div
+            variants={iconVariants(4)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 "
+          >
+            <FaGithub className="text-7xl text-gray-500" />
+          </motion.div>
+          <motion.div
+            variants={iconVariants(4)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 "
+          >
+            <VscVscode
+ className="text-7xl text-blue-400" />
+          </motion.div>
+          <motion.div
+            variants={iconVariants(4)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 "
+          >
+            <FaWindows
+ className="text-7xl text-blue-400" />
+          </motion.div>
         </motion.div>
       </motion.div>
     </div>
