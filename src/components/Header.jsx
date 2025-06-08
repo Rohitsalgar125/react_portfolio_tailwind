c56bc46f8808e33b437adb92dae762a6fb425ad1
RohitSalgar <rohitadinathsalgar@allmasters.ai>
2025-06-09 01:16:37 +0530

    testing for aws

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,7 +23,7 @@ const Header = ({ darkMode, setDarkMode }) => {
             transition={{ duration: 0.5 }}
             className="text-xl font-bold text-indigo-600 dark:text-indigo-400"
           >
-            Rohit Salgar
+            Rohit S
           </motion.a>
           
           <nav className="hidden md:flex space-x-8">
